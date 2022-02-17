227c3511ca7a564e88fa0a546b8207f34dd9d4f7
Matheus Cadena <matheus8emidio@gmail.com>
2022-02-17 16:07:06 -0500

    Designed the view and splash screen

---
+++ app/src/main/java/com/matheus/currencyexchangeapp/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.matheus.currencyexchangeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                this.finish()
+            }, 3000
+        )
+    }
+}
